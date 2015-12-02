8b4b0a6c578a6b4280a50b4793ad5a246a5eaa7c
glab <liubin@glab.cn>
2015-12-02 08:51:26 +0800

    rename site model

--- models/site.js
+++ models/site.js
@@ -1,6 +1,6 @@
 var mongodb = require('./mongodb');
 var Schema = mongodb.mongoose.Schema;
-var siteSchema = new Schema({
+var SiteSchema = new Schema({
     name : String,
     alias : [String],
     publish : Date,
@@ -20,6 +20,6 @@ var siteSchema = new Schema({
         create_date : { type: Date, default: Date.now }
     }]
 });
-var siteModel = mongodb.mongoose.model("site", siteSchema);
+var siteModel = mongodb.mongoose.model("Site", SiteSchema);
 var siteDAO = function(){};
 module.exports = new siteDAO();
