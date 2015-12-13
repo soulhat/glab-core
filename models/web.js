f0b7ae0c45d1cdd833c1382fad1f7e37b8dd0e9d
glab <liubin@glab.cn>
2015-12-13 22:48:58 +0800

    modify model file

---
+++ models/web.js
@@ -0,0 +1,13 @@
+'use strict';
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+    email: String,
+    password: String,
+	phone: Number,
+	name: String,
+    token: String
+});
+
+module.exports = mongoose.model('User', UserSchema);
