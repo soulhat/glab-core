7faa31b9ba790efa2268ea38ff628c30bb73aa21
glab <liubin@glab.cn>
2015-12-14 22:07:51 +0800

    add coffee support in development gulp config

--- gulp/development.js
+++ gulp/development.js
@@ -1,3 +1,20 @@
 'use strict';
 
-var gulp = require('gulp')
+var gulp = require('gulp'),
+	coffee = require('gulp-coffee'),
+	gutil = require('gulp-util'),
+	paths = {
+		coffee: ['controllers/**/*.coffee']
+	};
+var defaultTasks = ['coffee', 'watch'];
+gulp.task('coffee', function(){
+	gulp.src(paths.coffee)
+		.pipe(coffee({bare: true}).on('error', gutil.log))
+		.pipe(gulp.dest('./controllers'));
+});
+
+gulp.task('watch', function(){
+	gulp.watch(paths.coffee, ['coffee']);
+})
+
+gulp.task('development', defaultTasks);
