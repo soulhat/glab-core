59a533920589abf7437d159e82a02efede4ed685
glab <liubin@glab.cn>
2015-12-30 23:22:17 +0800

    gulp-autoprefixer

--- gulp/utils.js
+++ gulp/utils.js
@@ -17,3 +17,15 @@ px2rem({
     remPrecision: 6         // rem precision (default: 6)
 })
 */
+
+gulp.task('autoprefixer', function () {
+    var postcss      = require('gulp-postcss');
+    var sourcemaps   = require('gulp-sourcemaps');
+    var autoprefixer = require('autoprefixer');
+ 
+    return gulp.src('./src/*.css')
+        .pipe(sourcemaps.init())
+        .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./dest'));
+});
