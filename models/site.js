f87acccdf06db56693dc2eda94b32b033cc4db87
glab <liubin@glab.cn>
2016-01-03 22:20:38 +0800

    mongodb schema

--- models/site.js
+++ models/site.js
@@ -2,12 +2,6 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var UserSchema = new Schema({
-    email: String,
-    password: String,
-	phone: Number,
-	name: String,
-    token: String
-});
+
 
 module.exports = mongoose.model('User', UserSchema);
