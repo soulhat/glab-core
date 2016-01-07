51af8f5d53235dd82700165390fb8cc827c49964
glab <liubin@glab.cn>
2016-01-07 21:40:54 +0800

    add site schema

--- models/site.js
+++ models/site.js
@@ -1,3 +1,10 @@
 'use strict';
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+    url: String,
+    slug: String,
+	label: Number,
+	age: String
+});
